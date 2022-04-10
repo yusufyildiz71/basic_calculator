8dc8df32a8a2f8fc70f7a92ffef1ec9db0d809fc
Yusuf Yıldız <yusufyildiz@Yusuf-MacBook-Air.local>
2022-04-11 00:10:54 +0300

    textfield update

--- lib/main.dart
+++ lib/main.dart
@@ -36,73 +36,92 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  var num1,num2,resultt;
-  TextEditingController t1=TextEditingController();
-  TextEditingController t2=TextEditingController();
+  var num1, num2, resultt;
+  TextEditingController t1 = TextEditingController();
+  TextEditingController t2 = TextEditingController();
 
-  Addition(){
+  Addition() {
     setState(() {
-      num1=num.parse(t1.text);
-      num2=num.parse(t2.text);
-      resultt=num1+num2;
-    });
-  }
-  Subtraction(){
-    setState(() {
-      num1=num.parse(t1.text);
-      num2=num.parse(t2.text);
-      resultt=num1-num2;
-    });
-  }
-  Multiplication(){
-    setState(() {
-      num1=num.parse(t1.text);
-      num2=num.parse(t2.text);
-      resultt=num1*num2;
-    });
-  }
-  Division(){
-    setState(() {
-      num1=num.parse(t1.text);
-      num2=num.parse(t2.text);
-      resultt=num1 ~/ num2;
+      num1 = num.parse(t1.text);
+      num2 = num.parse(t2.text);
+      resultt = num1 + num2;
     });
   }
 
+  Subtraction() {
+    setState(() {
+      num1 = num.parse(t1.text);
+      num2 = num.parse(t2.text);
+      resultt = num1 - num2;
+    });
+  }
+
+  Multiplication() {
+    setState(() {
+      num1 = num.parse(t1.text);
+      num2 = num.parse(t2.text);
+      resultt = num1 * num2;
+    });
+  }
+
+  Division() {
+    setState(() {
+      num1 = num.parse(t1.text);
+      num2 = num.parse(t2.text);
+      resultt = num1 ~/ num2;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(50.0),
       child: Center(
-          child: Column(
-        children: <Widget>[
-          Text("$resultt"),
-          TextField(
-            controller: t1,
-            decoration: InputDecoration(
-    border: OutlineInputBorder(),
-    labelText: "Number 1",
-    prefixIcon: Icon(Icons.one_k,color: Colors.black,),
-    
-  ),
-          ),
-          TextField(
-            controller: t2,
-            decoration: InputDecoration(
-    border: OutlineInputBorder(),
-    labelText: "Number 2",
-    prefixIcon: Icon(Icons.two_k,color: Colors.black,),
-    
-  ),
-          ),
-          RaisedButton(onPressed: Addition, child: Text("+"),),
-          RaisedButton(onPressed: Subtraction,child: Text("-"),),
-          RaisedButton(onPressed: Multiplication, child: Text("*"),),
-          RaisedButton(onPressed: Division, child: Text("/"),),
-
-        ],
-      ),
+        child: Column(
+          children: <Widget>[
+            Text("$resultt"),
+            TextField(
+              controller: t1,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Number 1",
+                prefixIcon: Icon(
+                  Icons.one_k,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            TextField(
+              controller: t2,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Number 2",
+                prefixIcon: Icon(
+                  Icons.two_k,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            RaisedButton(
+              onPressed: Addition,
+              child: Text("+"),
+            ),
+            RaisedButton(
+              onPressed: Subtraction,
+              child: Text("-"),
+            ),
+            RaisedButton(
+              onPressed: Multiplication,
+              child: Text("*"),
+            ),
+            RaisedButton(
+              onPressed: Division,
+              child: Text("/"),
+            ),
+          ],
+        ),
       ),
     );
   }
